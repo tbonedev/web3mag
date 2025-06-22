b9b7374634d50ebd414b36fdeb41d69847c821f3
tbone <tufytop@gmail.com>
2025-06-21 19:08:20 -0700

    feat(auth): "initialize module"

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SessionEntity } from './entities/session.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SessionEntity])],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
