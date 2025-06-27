67dab97abeb9483eba9d4cbd7669bd7567192191
tbone <tufytop@gmail.com>
2025-06-26 20:18:33 -0700

    feat(auth): "implemented controller and service"

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { GetCurrentUser } from 'src/decorators/get-current-user.decorator';
+import { Post, Controller, Body } from '@nestjs/common';
+import { IsPublic } from 'src/decorators/public.decorator';
+import { LoginReqDto } from './dto/login-req.dto';
+import { LoginResDto } from './dto/login-res.dto';
+import { RefreshReqDto } from './dto/refresh-req.dto';
+import { RefreshResDto } from './dto/refresh-res.dto';
+import { RegisterReqDto } from './dto/register.req.dto';
+import { RegisterResDto } from './dto/register.res.dto';
+import { JwtPayloadType } from './types/jwt-payload.type';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @IsPublic()
+  @Post('email/login')
+  async signIn(@Body() dto: LoginReqDto): Promise<LoginResDto> {
+    return await this.authService.signin(dto);
+  }
+  @IsPublic()
+  @Post('email/register')
+  async register(@Body() dto: RegisterReqDto): Promise<RegisterResDto> {
+    return await this.authService.register(dto);
+  }
+
+  @Post('logout')
+  async logout(@GetCurrentUser() userToken: JwtPayloadType): Promise<void> {
+    await this.authService.logout(userToken);
+  }
+  @IsPublic()
+  @Post('refresh')
+  async refresh(@Body() dto: RefreshReqDto): Promise<RefreshResDto> {
+    return await this.authService.refreshToken(dto);
+  }
+}
